a2b45b67ee751a757d0def3db79d9a35ac1a86f0
n0259443 <n0259443@VDDK03X-4CEIMXZ.lm.lmig.com>
2018-10-23 09:32:42 -0400

    rest assured draft version

---
+++ RestAssured/ResAss.bat
@@ -0,0 +1,5 @@
+cd C:\Users\n0259443\eclipse-workspace\RestAssured 
+java -cp C:\Users\n0259443\eclipse-workspace\RestAssured\lib\*;C:\Users\n0259443\eclipse-workspace\RestAssured\bin
+mvn clean test -DsuiteXmlFile=testng.xml
+
+pause
---
+++ RestAssured/src/test/java/com/demo/RestAssured/SampleRequest.java
@@ -0,0 +1,57 @@
+package com.demo.RestAssured;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+ 
+public class SampleRequest {
+ 
+	 	@Test
+	    public void restAssuredTestBDDFramwork() {
+		 System.out.println("Started restAssuredTestBDDFramwork from here");
+	       
+		  given().auth().basic("n0259443", "Badvel@1").
+	            when().
+	            	get("https://test1-lmbc-empr-mlc-tds-service.us-east-1.np.paas.lmig.com/services/v1/tds/all").
+	            then().
+	            	assertThat().
+	            	statusCode(200).
+	            	and().
+	            	body("transDataResources", hasSize(6));/*.
+	            	and().
+	            	body("cotent-length",equalTo("4551"))*/;
+	            	            	
+	            	//-----------------------
+	            	
+	            	System.out.println("restAssuredTestBDDFramwork End here");
+	            
+	    }
+	 	
+	 	@Test (priority =1)
+	    public void SampleDemorestAssuredTest() {
+	 		System.out.println("Started restAssuredTestSampleDemo from here");
+	 		RestAssured.baseURI = "https://test1-lmbc-empr-mlc-tds-service.us-east-1.np.paas.lmig.com/services/v1/tds";
+        	RequestSpecification httpRequest = RestAssured.given().auth().basic("n0259443", "Badvel@1");
+        	Response response = httpRequest.get("/all");
+         
+        	// Retrieve the body of the Response
+        	ResponseBody body = response.getBody();
+         
+        	// By using the ResponseBody.asString() method, we can convert the  body
+        	// into the string representation.
+        	System.out.println("Response Body is: " + body.asString());
+        	System.out.println("restAssuredTestSampleDemo End here");
+	 		
+	 	}
+
+		
+ 
+}
